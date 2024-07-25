8bd4011286a0773530f75a43376e778791ec33c7
Scheibelhofer Thomas <ScheibeT@kapsch.net>
2024-07-25 13:09:22 +0200

    change response obect

--- src/main/java/org/example/jsmpocrestservice/rest/controller/AssetController.java
+++ src/main/java/org/example/jsmpocrestservice/rest/controller/AssetController.java
@@ -3,6 +3,7 @@ package org.example.jsmpocrestservice.rest.controller;
 import lombok.extern.log4j.Log4j2;
 import org.example.jsmpocrestservice.rest.exception.NotAuthorizedException;
 import org.example.jsmpocrestservice.rest.model.Asset;
+import org.example.jsmpocrestservice.rest.model.AssetResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,18 +19,29 @@ import java.util.List;
 public class AssetController {
 
     @GetMapping(produces = "application/json; charset=utf-8")
-    public ResponseEntity<List<Asset>> getAssets(@RequestParam(defaultValue = "100") int numberOfAssets,
-                                                 @RequestParam(required = false) String token) {
-
-//        if (!token.equals("1234")) {
-//            throw new NotAuthorizedException("Invalid token");
-//        }
+    public ResponseEntity<AssetResponse> getAssets(@RequestParam(defaultValue = "100") int numberOfAssets,
+                                                   @RequestParam(required = false) String token) {
 
         List<Asset> assetList = new ArrayList<>(numberOfAssets);
         for (int i = 0; i < numberOfAssets; i++) {
             assetList.add(new Asset(i, "asset" + i));
         }
-        return ResponseEntity.ok(assetList);
+        return ResponseEntity.ok(new AssetResponse(assetList));
+
+    }
+
+    @GetMapping(value = "/auth", produces = "application/json; charset=utf-8")
+    public ResponseEntity<AssetResponse> getAssetsWithAuth(@RequestParam(defaultValue = "100") int numberOfAssets,
+                                                           @RequestParam(required = false) String token) {
+        if (!token.equals("1234")) {
+            throw new NotAuthorizedException("Invalid token");
+        }
+
+        List<Asset> assetList = new ArrayList<>(numberOfAssets);
+        for (int i = 0; i < numberOfAssets; i++) {
+            assetList.add(new Asset(i, "asset" + i));
+        }
+        return ResponseEntity.ok(new AssetResponse(assetList));
 
     }
 
---
+++ src/main/java/org/example/jsmpocrestservice/rest/model/AssetResponse.java
@@ -0,0 +1,6 @@
+package org.example.jsmpocrestservice.rest.model;
+
+import java.util.List;
+
+public record AssetResponse(List<Asset> assets) {
+}
